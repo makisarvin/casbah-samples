4aabc838a4d343f4f12714d097b531f97c7f8d1f
unknown <arvange@D02TN1300055W7.net1.cec.eu.int>
2013-08-29 15:11:18 +0200

    Added geo location query

--- src/main/scala/Queries.scala
+++ src/main/scala/Queries.scala
@@ -2,7 +2,6 @@ package com.arvanitis
 
 import com.mongodb.casbah.Imports._
 
-
 case class Trip(name: String, tags: List[String], country: String)
 
 
@@ -26,25 +25,69 @@ object Queries extends App {
 
   mongoColl += tripDb2
 
-  //now build the query
+  //now build the queries
 
+  //1st query. Search for a single value in tags
+  val query_simple = MongoDBObject("tags" -> "sun")
 
- // val query = MongoDBObject("tags" -> "sun")
+  println("search for a single value in tags")
+  for (x <- mongoColl.find(query_simple)) println(x)
+
+  //2nd query. Search for multiple values in tags. This will search any value in the list
   val params: List[String] = List("sun", "food")
-  //val query = ("tags" $in ("sun", "food"))
-  val query = ("tags" $in params)
+  val query_mult_1 = ("tags" $in params)
+  val query_mult_2 = ("tags" $in ("sun", "eiffel"))
 
-  println("results ")
-  val trips = for (x <- mongoColl.find(query)) println(x)
+  val params_empty = List[String]()
+  val query_mult_3 = ("tags" $in params_empty)
 
+  println("Search for multiple values in tags ")
+
+  println("mult_1. Should return Crete")
+  for (x <- mongoColl.find(query_mult_1)) println(x)
+
+  println("mult_2. Should return both")
+  for (x <- mongoColl.find(query_mult_2)) println(x)
+
+  println("mult_3. Should be empty")
+  for (x <- mongoColl.find(query_mult_3)) println(x)
+
+  //3rd query. Search for tags and country
+
+  val query_complex_1 = $and { "country" -> "Greece" :: ("tags" $in ("sun", "eiffel")) }
+  println("complex_1. Should be Crete")
+  for (x <- mongoColl.find(query_complex_1)) println(x)
+
+  //4th query. Geolocation
+  val query_geo_1 = $and { ("country_loc" $near (50,50)) :: ("tags" $in ("sun", "eiffel")) }
+  println("complex_1. Should be both")
+  //for (x <- mongoColl.find(query_geo_1)) println(x)
+
+  val params_geo = MongoDBObject( "country_loc" ->
+    MongoDBObject("$near" ->
+      MongoDBObject("$geometry" ->
+        MongoDBObject("type" -> "Point",
+          "coordinates" -> GeoCoords(50.12, 50.12)),
+        "$maxDistance" -> 100)))
+
+  val query_geo_2 = ("country_loc" $nearSphere  (50, 50))
+  for (x <- mongoColl.find(query_geo_2)) println(x)
+  println("----------TEST------------------")
+  for (x <- mongoColl.find(params_geo)) println(x)
 
 
 
   def writer(trip: Trip): DBObject = {
+
+    val location =  MongoDBObject.newBuilder
+    location += "type" -> "Point"
+    location += "coordinates" -> GeoCoords(50.12, 50.12)
+
     val builder = MongoDBObject.newBuilder
     builder += "name" -> trip.name
     builder += "tags" -> trip.tags
     builder += "country" -> trip.country
+    builder += "country_loc" -> location.result
 
     builder.result()
   }
